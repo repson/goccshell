6778f3f0560dbdb1285f411840759932179074dd
Isaac Peña <info@ipenat.com>
2024-11-30 00:05:50 +0100

    Add the simple shell

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Conding Challenges Shell")
+	// Display the prompt
+	fmt.Print("ccsh> ")
+
+	// Read a line of input from the user
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	// Prepare the Command to be run, capturing the input
+	cmd := exec.Command(input)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
 }
