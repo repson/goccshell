a2397ddfaff72d2c0971b909d805e7f58434c618
Isaac Peña <info@ipenat.com>
2024-12-03 22:40:06 +0100

    Add handle exiting from the shell, multiple commands, non existent commands and arguments

--- main.go
+++ main.go
@@ -9,18 +9,37 @@ import (
 )
 
 func main() {
-	// Display the prompt
-	fmt.Print("ccsh> ")
+	for {
+		// Display the prompt
+		fmt.Print("ccsh> ")
 
-	// Read a line of input from the user
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
+		// Read a line of input from the user
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
 
-	// Prepare the Command to be run, capturing the input
-	cmd := exec.Command(input)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+		// If the user didn't type anything, loop again
+		if input == "" {
+			continue
+		}
 
-	cmd.Run()
+		parts := strings.Fields(input)
+		var command = parts[0]
+		var args = parts[1:]
+
+		// Exit the shell if the user types "exit"
+		if input == "exit" {
+			os.Exit(0)
+		}
+
+		// Prepare the Command to be run, capturing the input
+		cmd := exec.Command(command, args...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
